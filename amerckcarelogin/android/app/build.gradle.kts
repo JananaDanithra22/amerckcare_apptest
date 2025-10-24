98e0dda3671eda57c7bbf3a1c08a57ba00995e9f
Janana <140499994+JananaDanithra22@users.noreply.github.com>
2025-10-24 15:33:39 +0530

    initializing firebase in flutter

--- amerckcarelogin/android/app/build.gradle.kts
+++ amerckcarelogin/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.amerckcarelogin"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.amerckcarelogin"
--- amerckcarelogin/lib/main.dart
+++ amerckcarelogin/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart'; // <-- added
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart'; // <-- added
 import 'providers/authprovider.dart'; // <-- corrected
 import 'screens/loginscreen.dart';
 import 'screens/homescreen.dart';
